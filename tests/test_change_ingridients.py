31c759ebe14ed39f439b716764827562dd0356f0
TarasevichLubov <infolove@mail.ru>
2023-12-04 08:40:32 +0700

    correct_error

--- tests/test_change_ingridients.py
+++ tests/test_change_ingridients.py
@@ -6,8 +6,7 @@ class TestBuilder:
     # Переход к разделу "Булки"
     def test_user_click_bred(self, driver):
         driver.get("https://stellarburgers.nomoreparties.site/")
-        if 'tab_tab_type_current__2BEPc' in driver.find_element(By.XPATH, page_brad).get_attribute('class'):
-            driver.find_element(By.XPATH, page_souse).click()
+        driver.find_element(By.XPATH, page_souse).click()
         driver.find_element(By.XPATH, page_brad).click()
         selected_class = driver.find_element(By.XPATH, page_brad).get_attribute('class')
         assert 'tab_tab_type_current__2BEPc' in selected_class
@@ -15,8 +14,6 @@ class TestBuilder:
     # Переход к разделу "Соусы"
     def test_user_click_souse(self, driver):
         driver.get("https://stellarburgers.nomoreparties.site/")
-        if 'tab_tab_type_current__2BEPc' in driver.find_element(By.XPATH, page_souse).get_attribute('class'):
-            driver.find_element(By.XPATH, page_brad).click()
         driver.find_element(By.XPATH, page_souse).click()
         selected_class = driver.find_element(By.XPATH, page_souse).get_attribute('class')
         assert 'tab_tab_type_current__2BEPc' in selected_class
@@ -24,8 +21,6 @@ class TestBuilder:
     # Переход к разделу "Начинки"
     def test_user_click_filling(self, driver):
         driver.get("https://stellarburgers.nomoreparties.site/")
-        if 'tab_tab_type_current__2BEPc' in driver.find_element(By.XPATH, page_filling).get_attribute('class'):
-            driver.find_element(By.XPATH, page_brad).click()
         driver.find_element(By.XPATH, page_filling).click()
         selected_class = driver.find_element(By.XPATH, page_filling).get_attribute('class')
         assert 'tab_tab_type_current__2BEPc' in selected_class
